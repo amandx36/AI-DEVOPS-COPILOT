addac1035042f7d2b0a9656bc2ea097924ce95c8
amandx36 <xamandeep360@gmail.com>
2025-07-20 23:21:24 +0530

    system info functionality added

--- main.py
+++ main.py
@@ -3,7 +3,12 @@
 # # use ollama for the local run and also open ai for the online brother 
 
 
+import system_info
 import typer       #  for the cli integration !! 
+from commands_online import explain_command_online     # if online mode than import the explain online command module
+from commands_offline import explain_command_offline            # else load  explain the offline mode command
+
+from system_info import get_system_information 
 
 app = typer.Typer()      # module to read the text from the terminal after the python main.py "ls -a "
 
@@ -11,21 +16,31 @@ app = typer.Typer()      # module to read the text from the terminal after the p
 @app.command()
 def main(
     mode: int = typer.Option(..., prompt="select the modoe 1 for online and 0 for offline "),
-    task: str = typer.Option(..., prompt="what you have to perform example [explain ,  adding !!!    ]"),
+    task: str = typer.Option(..., prompt="what you have to perform example [explain , systeminfo!!!    ]"),
 ):
     if mode == 1:
         print("Enter into the online mode ")
-        from commands_online import explain_command_online as explain_command    # if online mode than import the explain online command module 
+        explain_command = explain_command_online    
     else:
         print("Entered into the offline mode ")
-        from commands_offline import explain_command_offline as explain_command     # else load  explain the offline mode command 
+        explain_command  =  explain_command_offline    
 
     if task == "explain":
         cmd = typer.prompt("command to explain?")
         out = explain_command(cmd)              # to take the input from the user 
         print(out)
+
+    elif task =="system_info" :                     # for getting the system information   !!! 
+        show = get_system_information()
+        print(show)
+
+    
+   
+    
     else:
         print("task not found.. only 'explain' works rn")
+
+
 
 if __name__ == "__main__":
     app()
---
+++ system_info.py
@@ -0,0 +1,21 @@
+#  this file is used to get the information from the system brother !! 
+
+
+import psutil    # this is the library used to get the system information !!! 
+
+
+def get_system_information():
+    cpu_usage = psutil.cpu_percent(interval= 1)     # to get the cpu usage
+    memory_usage = psutil.virtual_memory().percent        # to get the information about the memory usage 
+    disk_usage = psutil.disk_usage('/').percent   # to get the disk usage 
+    cpu_cores = psutil.cpu_count(logical=True)           # to get the cpu core usage !! 
+   
+    swap_memory = psutil.swap_memory().percent    # use to get the swap memory usage 
+
+    information  =  f"CPU usage of your system is {cpu_usage}%, \n Memory Usage is {memory_usage}%, \n Disk usage is {disk_usage}%, \n  Number of CPU cores in your system is {cpu_cores}, \n Swap memory usage is {swap_memory}%  "
+    
+    
+    
+    
+
+    return information 
